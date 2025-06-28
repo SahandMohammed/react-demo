33eb30dba53ab212dfa55eb778deae610412a89e
SahandMohammed <snqiu210234@uniq.edu.iq>
2025-06-29 01:06:58 +0300

    SCDA-2: Update footer copyright to reflect new company name

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -151,7 +151,7 @@ const Login = () => {
 
         {/* Footer */}
         <div className="text-center mt-8 text-gray-600 text-sm">
-          <p>© 2025 ERP System. All rights reserved.</p>
+          <p>© 2025 LogicBloom. All rights reserved.</p>
         </div>
       </div>
     </div>
